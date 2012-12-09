93737977485cf537c6c88ed59e110d2d51a7627b
Christian Kalhauge <christian@kalhauge.dk>
2012-12-09 19:20:44 +0100

    Added the framebuffer class

---
+++ lib/handlers/ECpp3dFramebuffer.h
@@ -0,0 +1,21 @@
+/*
+ * ECpp3dFramebuffer.h
+ *
+ *  Created on: 09/12/2012
+ *      Author: christian
+ */
+
+#ifndef ECPP3DFRAMEBUFFER_H_
+#define ECPP3DFRAMEBUFFER_H_
+
+namespace ECpp3d {
+
+class Framebuffer {
+
+};
+
+}
+
+
+
+#endif /* ECPP3DFRAMEBUFFER_H_ */
